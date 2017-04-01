6e6695679ec01fd53fa684810f24cd7c3a9fb6ac
wangyw <13709821410@139.com>
2017-04-01 17:34:28 +0800

    Initial commit

---
+++ home_application/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from common.mymako import render_mako_context
+
+
+def home(request):
+    """
+    首页
+    """
+    return render_mako_context(request, '/home_application/home.html')
+
+
+def dev_guide(request):
+    """
+    开发指引
+    """
+    return render_mako_context(request, '/home_application/dev_guide.html')
+
+
+def contactus(request):
+    """
+    联系我们
+    """
+    return render_mako_context(request, '/home_application/contact.html')
+def index(request):
+    """
+    新版首页-王有为
+    """
+    return render_mako_context(request, '/home_application/index.html')
